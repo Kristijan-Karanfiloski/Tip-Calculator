3f6f69653793a2dfe72a20c396062e95b34a3f54
Kristijan Karanfiloski <kikaccc29@gmail.com>
2023-09-07 09:38:51 +0200

    improved the formula for tipPerPerson and totalTip

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -1,28 +1,24 @@
 import styles from "./Calculator.module.css";
 
 const Calculator = ({ data, handleReset }) => {
-  const calculateTip = (tip, bill) => {
-    if (!tip || isNaN(tip) || tip === 0) return 0;
-    return (tip / 100) * bill;
-  };
-  const tipOnTotalAmount = calculateTip(Number(data.tip), Number(data.bill));
+  // const calculateTip = (tip, bill) => {
+  //   if (!tip || isNaN(tip) || tip === 0) return 0;
+  //   return (tip / 100) * bill;
+  // };
+  // const totalAmount = Number(data.bill) + tipAmount;
+  const tipAmountPerPerson =
+    (Number(data.bill) / Number(data.people)) * (Number(data.tip) / 100);
+  const totalPerPerson =
+    tipAmountPerPerson + Number(data.bill) / Number(data.people);
 
-  // const tipOnTotalAmount =
-  //   data.tip === "" ? 1 : ((data.tip * 1) / 100) * data.bill;
+  // console.log("Tip Amount :", tipAmount);
+
+  // const tipAmountPerPerson = (Number(data.bill)/data.people)*()
 
   // const tipPerPerson =
-  //   Number(data.people) !== 0 && !isNaN(Number(data.people))
+  //   Number(data.people) > 0 && !isNaN(Number(data.people))
   //     ? tipOnTotalAmount / Number(data.people)
   //     : 0;
-
-  const tipPerPerson =
-    Number(data.people) > 0 && !isNaN(Number(data.people))
-      ? tipOnTotalAmount / Number(data.people)
-      : 0;
-
-  // const tipPerPerson =
-  // data.people && data.people !== 0 ? tipOnTotalAmount / data.people : 0;
-  // const tipPerPerson = data.people ? tipOnTotalAmount / data.people : 0;
 
   return (
     <div className={`${styles.tipWrapper} ${styles.gridFlow}`}>
@@ -32,14 +28,23 @@ const Calculator = ({ data, handleReset }) => {
           <p className={styles.bottomParagraph}>/ person</p>
         </div>
 
-        <span>$ {tipPerPerson.toFixed(2)}</span>
+        <span>
+          {/*${" "}*/}
+          {`$${
+            isFinite(tipAmountPerPerson)
+              ? tipAmountPerPerson.toFixed(2)
+              : "0.00"
+          }`}
+        </span>
       </div>
       <div className={styles.total}>
         <div>
           <p className={styles.topParagraph}>Total</p>
           <p className={styles.bottomParagraph}>/ person</p>
         </div>
-        <span>$ {tipOnTotalAmount.toFixed(2)}</span>
+        <span>{`$${
+          isFinite(totalPerPerson) ? totalPerPerson.toFixed(2) : "0.00"
+        }`}</span>
       </div>
       <button onClick={handleReset}>RESET</button>
     </div>
